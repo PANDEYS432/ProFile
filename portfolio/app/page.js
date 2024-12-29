26394c8b83bab0ac8134afb5ac6159f2601f4e22
PANDEYS432 <98703881+PANDEYS432@users.noreply.github.com>
2024-12-29 15:16:47 +0530

    project setup and dashboard creation
    The the Next.js project has been setup and dashboard has been created

---
+++ portfolio/app/page.js
@@ -0,0 +1,12 @@
+
+import Dashboard from "./components/dashboard";
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+    
+    <div className="container mt-24 mx-auto px-12 py-4">
+      <Dashboard />
+    </div>
+  </main>
+  );
+}
